bc30d7860499734f19d7e5331fc585638a7e5558
pdtc <pablo.trinidadcarhuas@gmail.com>
2023-04-30 21:23:48 -0500

    first commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+    @GetMapping("/{greeting}")
+    public String helloWorld (@PathVariable String greeting) {
+        return greeting;
+    }
+}
